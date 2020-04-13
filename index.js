16be41a813de0789f3c126fd084884fab027ae73
Michael Fursa <bellamy9678@yahoo.com>
2020-04-13 18:59:47 +0300

    feat: add prod version

--- index.js
+++
@@ -1,8 +0,0 @@
-import CreateDomElements from './src/CreateDomElements';
-import VirtualKeyboard from './src/VirtualKeyboard';
-
-const application = new CreateDomElements();
-document.querySelector('body').appendChild(application.createDom());
-
-const virtualKeyboard = new VirtualKeyboard();
-virtualKeyboard.init().start();
